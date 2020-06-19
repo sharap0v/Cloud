6b243f13e8f3dfc453645327cdebdc0a48aca4be
Sergey Zelentsov <_sharapov_@mail.ru>
2020-06-19 10:27:21 +0300

    Init Cloud Project Preliminary Gui

---
+++ src/sample/LoginController.java
@@ -0,0 +1,13 @@
+package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+}
